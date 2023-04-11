02b42fa3407ad51fee35cee7d5019720dc1ae655
Lars Kruse <lakruzz@inc-inc.dk>
2023-04-11 15:02:19 +0200

    Added support for init sql

--- dockerfile
+++ dockerfile
@@ -3,6 +3,9 @@ FROM eclipse-temurin:20-jre-jammy
 LABEL author="Lakruzz <lars@lakruzz.com>"
 LABEL maintainer="Lakruzz <lars@lakruzz.com>"
 
+# This is the default password for the root user
+ENV MYSQL_ROOT_PASSWORD=root 
+
 RUN apt-get update && apt upgrade -y
 
 RUN apt-get install -y \
@@ -29,5 +32,14 @@ RUN useradd --no-log-init -r -g staff vscode
 
 COPY mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 
+RUN mkdir /docker-entrypoint-initdb.d || true
+
+RUN mkdir /lamj || true
+COPY lamj.init.sh /lamj/
+
 WORKDIR /app
-EXPOSE 8080 8081 3306 3307 33060
+EXPOSE 8080 3306
+
+CMD /lamj/lamj.init.sh && \
+    tail -f /dev/null
+ 
---
+++ lamj.init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+service mysql start 
+sleep 3
+
+echo
+echo " * Create use 'root'@'%' grant all privileges and and set password"
+mysql -u root -e "CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+
+echo
+echo " * Running init scripts from /docker-entrypoint-initdb.d"
+
+if [ -n "$(ls -A /docker-entrypoint-initdb.d/*.sql 2>/dev/null)" ]; then
+    for script in $(ls /docker-entrypoint-initdb.d/*.sql | sort -n); do
+        echo "   * Executing $script "
+        mysql -uroot  --silent < "$script"
+    done
+else
+    echo " * No SQL scripts found in /docker-entrypoint-initdb.d directory"
+fi
+
+echo
+echo " * Set password on 'root'@'localhost'"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES;"   
